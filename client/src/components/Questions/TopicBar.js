9bd4ab4f4f88e0407c923340a07b121d5d0d1b37
Justus Soh <justus.soh@hotmail.com>
2020-04-02 12:39:24 +0800

    Added Styles for Topic Bar

--- client/src/components/Questions/TopicBar.js
+++ client/src/components/Questions/TopicBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {LinkContainer} from 'react-router-bootstrap'
 import {Container, Nav} from "react-bootstrap";
+import './TopicBar.css'
 
 class TopicBar extends React.Component {
 
@@ -8,26 +9,23 @@ class TopicBar extends React.Component {
         return (
             <Container>
                 <Nav className="flex-column">
-                    <LinkContainer exact to="/"
-                                   activeStyle={{
-                                       fontWeight: "bold",
-                                       color: "red"
-                                   }}>
-                        <Nav.Item>All Questions</Nav.Item>
-                    </LinkContainer>
-                    <Nav.Item disabled>Featured Topics</Nav.Item>
+                    <div className='list-item-container' style={{marginBottom: 10}}>
+                        <LinkContainer exact to="/">
+                            <Nav.Item className='list-item list-item-text list-item-text-hover'>All Questions</Nav.Item>
+                        </LinkContainer>
+                    </div>
+                    <div className="list-item-container">
+                        <Nav.Item disabled className='list-item list-item-text' style={{fontWeight: 'bold'}}>Featured
+                            Topics</Nav.Item>
+                        {this.props.topicList.map((topic, index) => {
+                            return (
 
-                    {this.props.topicList.map((topic, index) => {
-                        return (
-                            <LinkContainer to={`/topic/${topic.key}`} key={index}
-                                           activeStyle={{
-                                               fontWeight: "bold",
-                                               color: "red"
-                                           }}>
-                                <Nav.Item>{topic.label}</Nav.Item>
-                            </LinkContainer>
-                        )
-                    })}
+                                <LinkContainer to={`/topic/${topic.key}`}>
+                                    <Nav.Item className='list-item list-item-text list-item-text-hover'>{topic.label}</Nav.Item>
+                                </LinkContainer>
+                            )
+                        })}
+                    </div>
                 </Nav>
             </Container>
         )
