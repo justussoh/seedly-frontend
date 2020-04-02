fefffd5dcb281086d756858bada7d5163cbc88c7
Justus Soh <justus.soh@hotmail.com>
2020-04-02 11:48:01 +0800

    Added Time Truncatting

--- client/src/components/Question/TimeComponent.js
+++ client/src/components/Question/TimeComponent.js
@@ -1,11 +1,38 @@
 import React from "react";
 
+const SECONDS = 1000;
+const MINUTES = SECONDS * 60;
+const HOURS = MINUTES * 60;
+const DAYS = HOURS * 24;
+const WEEKS = DAYS * 7;
+const MONTHS = WEEKS * 4;
+const YEARS = MONTHS * 12;
+
+
 class TimeComponent extends React.Component {
+
+    getTimeString = (timeAnswered) => {
+        let diff = new Date() - Date.parse(timeAnswered);
+        if (diff < HOURS) {
+            return `${Math.ceil(diff / MINUTES)}m`
+        } else if (diff < DAYS) {
+            return `${Math.floor(diff / HOURS)}h`
+        } else if (diff < WEEKS) {
+            return `${Math.floor(diff / DAYS)}d`
+        } else if (diff < MONTHS) {
+            return `${Math.floor(diff / WEEKS)}w`
+        } else if (diff < YEARS) {
+            return `${Math.floor(diff / MONTHS)}m`
+        } else{
+            return `${Math.floor(diff / YEARS)}y`
+
+        }
+    };
 
     render() {
         return (
             <div>
-                <h6>{this.props.timeAnswered}</h6>
+                <h6>Answered {this.getTimeString(this.props.timeAnswered)} ago</h6>
             </div>
         )
     }
--- server.js
+++ server.js
@@ -5,55 +5,153 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api', (req, res) => {
-    res.send({ status: 'OK' });
+    res.send({status: 'OK'});
 });
 
 app.get('/api/topic_list', (req, res) => {
-    res.send({ topics: [
+    res.send({
+        topics: [
             {
-                key:'milelion',
+                key: 'milelion',
                 label: 'MileLion'
             },
             {
                 key: 'stock_discussion',
-                label:'Stock Discussion'
+                label: 'Stock Discussion'
             },
             {
-                key:'investments',
+                key: 'investments',
                 label: 'Investments'
             }
-        ] });
+        ]
+    });
+});
+
+app.get('/api/level_list', (req, res) => {
+    res.send({
+        levels: [
+            {
+                level: 1,
+                label: 'WonderKid'
+            },
+            {
+                level: 2,
+                label: 'WonderKid'
+            },
+            {
+                level: 3,
+                label: 'WonderKid'
+            },
+            {
+                level: 4,
+                label: 'WonderKid'
+            },
+            {
+                level: 5,
+                label: 'WonderKid'
+            },
+            {
+                level: 6,
+                label: 'WonderKid'
+            },
+            {
+                level: 7,
+                label: 'WonderKid'
+            },
+            {
+                level: 8,
+                label: 'WonderKid'
+            },
+            {
+                level: 9,
+                label: 'WonderKid'
+            },
+            {
+                level: 10,
+                label: 'WonderKid'
+            },
+
+
+        ]
+    });
 });
 
 app.get('/api/status_list', (req, res) => {
-    res.send({ statuses: [
+    res.send({
+        statuses: [
             {
-                key:'recent_activity',
+                key: 'recent_activity',
                 label: 'Recent Activity'
             },
             {
                 key: 'unanswered',
-                label:'Unanswered'
+                label: 'Unanswered'
             },
             {
-                key:'trending',
+                key: 'trending',
                 label: 'Trending'
             }
-        ] });
+        ]
+    });
 });
 
 app.get('/api/questions', (req, res) => {
-    res.send({ questions: [
+    let now = new Date();
+    now.setMonth(now.getMonth() - 3);
+    res.send({
+        questions: [
             {
-                category:['milelion', 'stock_discussion'],
-                status:['recent_activity', 'unanswered'],
-                title:'Test'
+                category: ['milelion', 'stock_discussion'],
+                status: ['recent_activity', 'unanswered'],
+                title: 'Test',
+                description: 'My thoughts too. However, I think it may be due to the fact that manicure, hair dressing, facial services are a want not a need. Probably that\'s why the government placed less emphasis on the particular sector. And as you mentioned, digital solutions are limited for such sector and Singapore is moving towards a digital future, they do thus have to prioritise their packages and budget.\n' +
+                    '\n' +
+                    'Overall, I think the recent package and initiative do benefit the whole Singapore regardless of sector. It is very much dependent on how you view the situation.\n' +
+                    '\n' +
+                    'Here\'s my 2 cents worth of view point.',
+                answer: ['Answer 1', 'Answer 2'],
+                answerTime: now,
+                userId: 1,
+                likes: [1, 2, 3],
             }
-        ] });
+        ]
+    });
 });
+
+app.get('/api/userId', (req, res) => {
+
+    let data = [
+        {
+            userId:1,
+            displayName:'Test 1',
+            level: 3,
+        },
+        {
+            userId:2,
+            displayName:'Test 2',
+            level: 7,
+        },
+        {
+            userId:3,
+            displayName:'Test 3',
+            level: 10,
+        },
+    ];
+
+    if (req.query.user) {
+        let userId = req.query.user;
+        data = data.filter(user=>userId.userId === userId);
+    }
+
+    res.send({
+        users: data,
+    });
+});
+
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
